a417062950edb902b5ab6a26a63cf50d0ce49b75
Christian Borges <christian.borges@kellogg.northwestern.edu>
2025-01-23 00:55:03 +0000

    DB Modifying Code

---
+++ school-5-insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "Christian",
+    "Borges",
+    "christian.borges@kellogg.northwestern.edu"
+);
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "Christian",
+    "Borges",
+    "christian.borges@kellogg.northwestern.edu"
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students 
+SET email = "c@cborges.co"
+WHERE id = 1;
